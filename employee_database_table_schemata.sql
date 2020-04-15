8a271ac750fb2917f0a1c3d899856a191d910680
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-04-15 16:56:47 -0700

    pushed ERD and table schema

---
+++ employee_database_table_schemata.sql
@@ -0,0 +1,88 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+
+--Imported CSV files into the corresponding SQL tables into employee_database in pgAdmin 4
+
+-- Delete the tables if they already exist
+DROP TABLE Departments;
+DROP TABLE Dept_Emp;
+DROP TABLE Dept_Manager;
+DROP TABLE Employees;
+DROP TABLE Salaries;
+DROP TABLE Titles;
+
+
+--Creating tables for employee_database
+CREATE TABLE Departments (
+    dept_no VARCHAR   NOT NULL,
+    dept_name VARCHAR   NOT NULL,
+    CONSTRAINT pk_Departments PRIMARY KEY (
+        dept_no
+     )
+);
+
+CREATE TABLE Dept_Emp (
+    emp_no INTEGER   NOT NULL,
+    dept_no VARCHAR   NOT NULL,
+    from_date DATE   NOT NULL,
+    to_date DATE   NOT NULL
+);
+
+CREATE TABLE Dept_Manager (
+    dept_no VARCHAR   NOT NULL,
+    emp_no INTEGER   NOT NULL,
+    from_date DATE   NOT NULL,
+    to_date DATE   NOT NULL
+);
+
+CREATE TABLE Employees (
+    emp_no INTEGER   NOT NULL,
+    birth_date DATE   NOT NULL,
+    first_name VARCHAR   NOT NULL,
+    last_name VARCHAR   NOT NULL,
+    gender VARCHAR   NOT NULL,
+    hire_date DATE   NOT NULL,
+    CONSTRAINT pk_Employees PRIMARY KEY (
+        emp_no
+     )
+);
+
+CREATE TABLE Salaries (
+    emp_no INTEGER   NOT NULL,
+    salary INT   NOT NULL,
+    from_date DATE   NOT NULL,
+    to_date DATE   NOT NULL
+);
+
+CREATE TABLE Titles (
+    emp_no INTEGER   NOT NULL,
+    title VARCHAR   NOT NULL,
+    from_date DATE   NOT NULL,
+    to_date DATE   NOT NULL
+);
+
+ALTER TABLE Dept_Emp ADD CONSTRAINT fk_Dept_Emp_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+ALTER TABLE Dept_Emp ADD CONSTRAINT fk_Dept_Emp_dept_no FOREIGN KEY(dept_no)
+REFERENCES Departments (dept_no);
+
+ALTER TABLE Dept_Manager ADD CONSTRAINT fk_Dept_Manager_dept_no FOREIGN KEY(dept_no)
+REFERENCES Departments (dept_no);
+
+ALTER TABLE Dept_Manager ADD CONSTRAINT fk_Dept_Manager_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+ALTER TABLE Salaries ADD CONSTRAINT fk_Salaries_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+ALTER TABLE Titles ADD CONSTRAINT fk_Titles_emp_no FOREIGN KEY(emp_no)
+REFERENCES Employees (emp_no);
+
+
+-- Query all fields from the tables
+SELECT * FROM Departments;
+SELECT * FROM Dept_Emp;
+SELECT * FROM Dept_Manager;
+SELECT * FROM Employees;
+SELECT * FROM Salaries;
+SELECT * FROM Titles;
