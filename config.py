4862fd7973e2ffffea35ef3b493cfeb290fa1edc
diannejardinez <45705176+diannejardinez@users.noreply.github.com>
2020-04-17 01:37:49 -0700

    updated salary code -connection

---
+++ config.py
@@ -0,0 +1,13 @@
+# Enter your Database Connection information below
+
+# Remember to delete sensitive information after using
+
+
+Username = "ENTER YOUR USERNAME"
+
+password = "ENTER YOUR PASSWORD"
+
+database = "ENTER DATABASE NAME" 
+
+
+
